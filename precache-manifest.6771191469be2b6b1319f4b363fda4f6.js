3401b786ff8287f8e0635b55ba680723468fcecc
MICHAEL NYAKPO <nyakpomichael@gmail.com>
2020-08-12 09:56:20 +0000

    Updates

---
+++ precache-manifest.6771191469be2b6b1319f4b363fda4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea1fc4bb5ba99e19602e99bc7a6d73d",
+    "url": "/nyakpomichael/my-app/index.html"
+  },
+  {
+    "revision": "9c89d8e4af12d28c05a4",
+    "url": "/nyakpomichael/my-app/static/css/main.d3e03414.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/nyakpomichael/my-app/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyakpomichael/my-app/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c89d8e4af12d28c05a4",
+    "url": "/nyakpomichael/my-app/static/js/main.698ff8fb.chunk.js"
+  },
+  {
+    "revision": "5fb10ee0143b1a15cf2b",
+    "url": "/nyakpomichael/my-app/static/js/runtime-main.f2d390c6.js"
+  }
+]);
